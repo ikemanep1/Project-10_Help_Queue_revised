2ece20e2c7f29eddbaad41cf6af42a1a3baa820e
Student <example@example.com>
2020-02-12 10:37:26 -0800

    add routing to project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Header(props){
   return (
@@ -12,6 +13,7 @@ function Header(props){
           color: purple;
         }
         `}</style>
+        <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
       </div>
     )
   }
